2d322b7720c6c8ad1c37cf74400ff5118201b75e
bekesi142 <bekesibalint2006@gmail.com>
2024-11-24 21:50:47 +0100

    Add project files.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    internal class Matrix
+    {
+        private int[,] matrixTomb;
+        private int sorok;
+        private int oszlopok;
+        private string nev;
+
+        public string Nev { get => nev; set => nev = value; }
+
+        public Matrix(int sorok, int oszlopok, string nev)
+        {
+            matrixTomb = new int[sorok, oszlopok];
+            this.sorok = sorok;
+            this.oszlopok = oszlopok;
+            this.nev = nev;
+        }
+
+        public static Matrix Osszeadas(Matrix a, Matrix b)
+        {
+            if (a.sorok != b.sorok || a.oszlopok != b.oszlopok)
+            {
+                throw new ArgumentException("A mátrixok méretének meg kell egyeznie");
+            }
+
+            Matrix eredmeny = new Matrix(a.sorok, a.oszlopok, "matrixC");
+            for (int i = 0; i < a.sorok; i++)
+            {
+                for (int j = 0; j < a.oszlopok; j++)
+                {
+                    eredmeny.matrixTomb[i, j] = a.matrixTomb[i, j] + b.matrixTomb[i, j];
+                }
+
+
+            }
+
+            return eredmeny;
+        }
+
+        public static void SzamHozzadasa(Matrix valasztottMatrix, int szam)
+        {
+            for (int i = 0; i < valasztottMatrix.sorok; i++)
+            {
+                for (int j = 0; j < valasztottMatrix.oszlopok; j++)
+                {
+                    valasztottMatrix.matrixTomb[i, j] += szam;
+                }
+            }
+
+        }
+
+        public void FeltoltesRandomszamokkal(int minimum, int maximum)
+        {
+            Random random = new Random();
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    matrixTomb[i, j] = random.Next(minimum, maximum + 1);
+                }
+            }
+        }
+
+        public void Kiiratas()
+        {
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    Console.Write(matrixTomb[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Matrix 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrixA = new Matrix(2, 3, "matrixA");
+            Matrix matrixB = new Matrix(2, 3, "matrixB");
+
+            matrixA.FeltoltesRandomszamokkal(1, 10);
+            matrixB.FeltoltesRandomszamokkal(1, 10);
+
+            Console.WriteLine("matrixA:");
+            matrixA.Kiiratas();
+            Console.WriteLine("\nmatrixB:");
+            matrixB.Kiiratas();
+
+            Console.WriteLine("\nKét matrix összege (matrixC):   ");
+            Matrix matrixC = Matrix.Osszeadas(matrixA, matrixB);
+            matrixC.Kiiratas();
+
+            List<Matrix> matrixList = new List<Matrix>() { matrixA, matrixB, matrixC };
+            
+            Console.Write("\nAdja meg melyik Matrix értékeit akarja növelni:  ");
+            string valasztottMatrix = Console.ReadLine();
+            Console.Write("Adja meg mennyivel szeretné növelni a választott Matrix értékeit:  ");
+            int valasztottNoveles = Convert.ToInt32(Console.ReadLine());
+
+
+            
+            for (int i = 0; i < matrixList.Count; i++)
+            {
+                if (matrixList[i].Nev == valasztottMatrix)
+                {
+                    Matrix.SzamHozzadasa(matrixList[i],valasztottNoveles);
+                    matrixList[i].Kiiratas();
+                }
+            }
+
+
+        }
+    }
+}
